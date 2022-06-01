e7eaba394e7844455e88c91ee0fd8f0d4e1cf874
Ashik045 <ashikurislam045@gmail.com>
2022-06-01 10:49:20 +0600

    new project

---
+++ pages/hotels/[hotel].js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const hotelDetails = () => <div>hotelDetails</div>;
+
+export default hotelDetails;
---
+++ pages/hotels/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const index = () => (
+    <div>
+        <h1>hotels</h1>
+    </div>
+);
+
+export default index;
