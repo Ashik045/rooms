d564597752da021f790431ce87b186f0b54e1b7d
Ashik045 <ashikurislam045@gmail.com>
2022-09-20 08:39:04 +0600

    fixing image  error

--- client/components/LovelyHome/LovelyHome.js
+++ client/components/LovelyHome/LovelyHome.js
@@ -5,8 +5,12 @@ import style from './LovelyHome.module.scss';
 
 function LovelyHome({ hmDetails }) {
     return (
-        <div className={style.lovely_home} style={{position: 'relative'}}>
-           {hmDetails?.images[0] && <Image className={style.lovely_home_img} src={hmDetails?.images[0]} alt="Lovely Hotel" width={300} height={300} layout="responsive" /> }
+        <div className={style.lovely_home} >
+            <div >
+                <Image className={style.lovely_home_img} src={hmDetails.images[0]} alt="Lovely Hotel" 
+                height={200} width={250} />
+            </div>
+           
             <h3>{hmDetails.name}</h3>
             <p>{hmDetails.city}</p>
             <p className={style.lovely_home_price}> Starting from {hmDetails.chipestprice}</p>
--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.pexels.com']
+    domains: ['www.pexels.com', 'images.pexels.com', 'unsplash.com', 'https://unsplash.com', 'firebasestorage.googleapis.com']
   }
 }
 
